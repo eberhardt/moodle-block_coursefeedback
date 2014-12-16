055ee9b351e22bdcbf6c3faeae2d154ad065c2f2
Jan Eberhardt <eberhardt@math.tu-berlin.de>
2014-12-16 15:26:24 +0100

    Update version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined("MOODLE_INTERNAL") || die();
 
-$plugin->version   = 2013070506;
+$plugin->version   = 2013070510;
 $plugin->requires  = 2012120304;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "1.0.6 (Build: 2013070506)";
+$plugin->release   = "1.0.6 (Build: 2013070510)";
 $plugin->component = "block_coursefeedback";
