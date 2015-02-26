b346ec713eac3a1b5c8287a7cb5c13349637fe06
Jan Eberhardt <eberhardt@math.tu-berlin.de>
2015-02-26 13:54:24 +0000

    Bugfix
    Changelog
       * Fixed PHP warning in `/lib.php`
       * Replaced all french characters with HTML entities
    
    	modified:   lang/de/block_coursefeedback.php
    	modified:   lang/en/block_coursefeedback.php
    	modified:   lang/fr/block_coursefeedback.php
    	modified:   lib.php
    	modified:   version.php

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 
 defined("MOODLE_INTERNAL") || die();
 
-$plugin->version   = 2013070511;
+$plugin->version   = 2013070512;
 $plugin->requires  = 2012120304;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "1.0.7 (Build: 2013070511)";
+$plugin->release   = "1.0.8 (Build: 2013070512)";
 $plugin->component = "block_coursefeedback";
