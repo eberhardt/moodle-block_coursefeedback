ff03a8fdde60e6d44a0864b1d22f3130644ff4cf
Felix Di Lenarda <dilenarda@tu-berlin.de>
2023-06-06 14:04:48 +0200

    Refactor and simplify ranking export

--- exportlib.php
+++ exportlib.php
@@ -29,6 +29,7 @@ defined("MOODLE_INTERNAL") || die();
 
 require_once($CFG->dirroot . "/blocks/coursefeedback/lib.php");
 require_once($CFG->libdir . '/csvlib.class.php');
+require_once(__DIR__ . "/locallib.php");
 
 
 /**
@@ -94,46 +95,34 @@ class feedback_exporter {
  * @copyright innoCampus, TU Berlin
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class rankingexport {
+class ranking_exporter {
     protected $feedback = null;
     protected $questionid = null;
     protected $csvexportwriter;
 
-    public function __construct($feedback = 0, $seperator = "\t") {
+    public function __construct($feedback = null, $question = null) {
         $this->csvexportwriter = new csv_export_writer();
     }
-
-    public function __construct($feedback = null, $question = null) {
+    public function create_file($feedbackid, $questionid = 0 )
+    {
         global $DB;
+        $this->csvexportwriter->set_filename(get_string("download_html_filename", "block_coursefeedback")
+            . date("_Y-m-d-H-i"));
 
-        if ($fb = $DB->get_record("block_coursefeedback", array("id" => $feedback))) {
-            $this->feedback = $fb;
-            $this->questionid = $question;
-        } else {
-            print_error("feedbacknotfound", "error");
-            exit(0);
-        }
-    }
-
-    public function export() {
-        global $DB;
-        $writer = new csv_export_writer('semicolon');
-        $writer->set_filename( clean_param(get_string("download_html_filename", "block_coursefeedback"),
-            PARAM_FILE));
-
-        $writer->add_data([
-            'Feedbackid: ' . $this->feedback->id,
-            'Feedbackid: ' . $this->feedback->name
+        $feedback = $DB->get_record("block_coursefeedback", ["id" => $feedbackid]);
+        $this->csvexportwriter->add_data([
+            'Feedbackid: ' . $feedback->id,
+            'Feedbackname: ' . $feedback->name
         ]);
 
         // Get questions
-        $qus = block_coursefeedback_get_questions_by_language($this->feedback->id, [current_language()]);
+        $qus = block_coursefeedback_get_questions_by_language($feedback->id, [current_language()]);
         $questions = null;
 
-        if ($this->questionid) {
+        if ($questionid != 0) {
             // Only display one question.
             foreach ($qus as $qu) {
-                if ($qu->questionid == $this->questionid) {
+                if ($qu->questionid == $questionid) {
                     $questions = array($qu);
                 }
             }
@@ -144,13 +133,13 @@ class rankingexport {
 
         foreach ($questions as $question) {
             // Output headings.
-            $writer->add_data([]);
-            $writer->add_data([
+            $this->csvexportwriter->add_data([]);
+            $this->csvexportwriter->add_data([
                 $question->question,
                 $question->questionid
             ]);
 
-            $writer->add_data([
+            $this->csvexportwriter->add_data([
                 get_string('course'),
                 get_string('user'),
                 get_string('name'),
@@ -164,200 +153,14 @@ class rankingexport {
                 get_string('notif_emoji_superbad', 'block_coursefeedback'),
                 get_string('table_html_average', 'block_coursefeedback'),
                 get_string('table_html_votes', 'block_coursefeedback'),
-                get_string('table_html_nochoice', 'block_coursefeedback'),
             ]);
 
-            // Get courseids and the amount of answers in this course for the current question.
-            $params = [
-                'questionid' => $question->questionid,
-                'feedbackid' => $this->feedback->id,
-                'answerlimit' => 0,
-                'feedbackid2' => $this->feedback->id,
-                'questionid2' => $question->questionid,
-            ];
-            $sql = "
-                SELECT one.courseid, five.usero, c.shortname, c.category, cc.path,  
-                       six.one, six.two, six.three, six.four, six.five, six.six, 
-                       (six.answersum / ( NULLIF ((one.anstotal - six.abstain), 0))) as average, 
-                       one.anstotal, six.abstain 
-                  FROM ( SELECT course as courseid, count(*) as anstotal 
-                           FROM {block_coursefeedback_answers}
-                          WHERE questionid = :questionid 
-                                AND coursefeedbackid = :feedbackid
-                       GROUP BY course
-                         HAVING count(*) > :answerlimit
-                       ) one
-             LEFT JOIN ( SELECT four.courseid, SUM(users) as usero 
-                           FROM ( SELECT e.id, e.courseid, two.users 
-                                    FROM {enrol} e
-                                    JOIN ( SELECT enrolid, COUNT(*) AS users 
-                                             FROM {user_enrolments}
-                                         GROUP BY enrolid 
-                                         ) two ON e.id = two.enrolid    
-                                ) four
-                         GROUP BY four.courseid  
-                       ) five ON five.courseid = one.courseid
-             LEFT JOIN {course} c ON one.courseid = c.id
-             LEFT JOIN {course_categories} cc ON cc.id = c.category
-             LEFT JOIN ( SELECT course, SUM(CASE WHEN answer = 0 THEN 1 ELSE 0 END) AS abstain, 
-                                SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) AS one,
-                                SUM(CASE WHEN answer = 2 THEN 1 ELSE 0 END) AS two,
-                                SUM(CASE WHEN answer = 3 THEN 1 ELSE 0 END) AS three,
-                                SUM(CASE WHEN answer = 4 THEN 1 ELSE 0 END) AS four,
-                                SUM(CASE WHEN answer = 5 THEN 1 ELSE 0 END) AS five,
-                                SUM(CASE WHEN answer = 6 THEN 1 ELSE 0 END) AS six,
-                                SUM(answer) AS answersum
-                          FROM {block_coursefeedback_answers}
-                         WHERE coursefeedbackid = :feedbackid2 
-                               AND questionid = :questionid2
-                      GROUP BY course
-                       ) six ON one.courseid = six.course";
-            $courses = $DB->get_records_sql($sql, $params);
-            array_walk($courses, function(&$e, $f) {
-                $e = get_object_vars($e);
-            });
-            foreach ($courses as $course) {
-                $writer->add_data($course);
-            }
-        }
-        $writer->download_file();
-    }
-
-    public function create_file($lang) {
-        global $DB;
-        $seperator = ";";
-        $newline = "\n";
-
-        $clang = current_language();
-        $this->content .= $this->feedback->name . $newline;
-        $this->content .= 'Feedbackid: ' . $this->feedback->id . $newline;
-
-        $qus = block_coursefeedback_get_questions_by_language($this->feedback->id, $clang);
-        $questions = null;
-
-        if ($this->questionid) {
-            // Only display one question.
-            foreach ($qus as $qu) {
-                if ($qu->questionid == $this->questionid) {
-                    $questions = array($qu);
-                }
-            }
-        } else {
-            // Display all questions.
-            $questions = $qus;
-        }
-        foreach ($questions as $question) {
-            // Output headings.
-            $this->content .= $question->question . ': ' . $question->questionid . $newline;
-            $this->content .= get_string('course') . $seperator . 'USER' . $seperator . get_string('name') . $seperator
-                . 'Category: ' . $seperator . 'TopLevelCategory' . $seperator
-                . get_string('notif_emoji_super', 'block_coursefeedback') . $seperator
-                . get_string('notif_emoji_good', 'block_coursefeedback') . $seperator
-                . get_string('notif_emoji_ok', 'block_coursefeedback') . $seperator
-                . get_string('notif_emoji_neutral', 'block_coursefeedback') . $seperator
-                . get_string('notif_emoji_bad', 'block_coursefeedback') . $seperator
-                . get_string('notif_emoji_superbad', 'block_coursefeedback') . $seperator
-                . get_string('table_html_average', 'block_coursefeedback') . $seperator
-                . get_string('table_html_votes', 'block_coursefeedback') . $seperator
-                . get_string('table_html_nochoice', 'block_coursefeedback') . $newline;
-
-            // Get courseids and the amount of answers in this course for the current question.
-            $params = [
-                'questionid' => $question->questionid,
-                'feedbackid' => $this->feedback->id,
-                'answerlimit' => 0,
-            ];
-            $sql = "SELECT course as courseid, count(*) 
-                      FROM {block_coursefeedback_answers}
-                     WHERE questionid = :questionid 
-                           AND coursefeedbackid = :feedbackid
-                  GROUP BY course
-                    HAVING count(*) > :answerlimit";
-
-            $courses = $DB->get_records_sql($sql, $params);
+            $courses = block_coursefeedback_get_courserankings($question->questionid, $feedbackid);
 
             foreach ($courses as $course) {
-
-                // Get amount of enrolled users for this course.
-                $usercount = 0;
-                $enrolmentinstances = $DB->get_records('enrol', array('courseid' => $course->courseid));
-                foreach ($enrolmentinstances as $einstance) {
-                    $params = array('enrolid' => $einstance->id);
-
-                    $sql = "SELECT enrolid,COUNT(*) AS count 
-                              FROM {user_enrolments}
-                             WHERE enrolid = :enrolid
-                          GROUP BY enrolid";
-
-                    if ($result = $DB->get_record_sql($sql, $params)) {
-                        $usercount += $result->count;
-                    }
-                }
-
-                // Get coursename und category (maybe the course is not available anymore).
-                // Output the informations.
-                try {
-                    $courseobj = get_course($course->courseid);
-                    // Output courseinfos.
-                    $category = \core_course_category::get($courseobj->category);
-                    $catpath = explode('/', $category->path);
-                    $this->content .= $course->courseid . $seperator . $usercount . $seperator . $courseobj->shortname . $seperator;
-                    // TODO: Müssen wir das vorangehende '/' überprüfen? => davon hängt die Richtigkeit von $catpath[1] ab
-                    $this->content .= $courseobj->category . $seperator . $catpath[1] . $seperator;
-                } catch (Exception $ex) {
-                    // Output alternative courseinfos.
-                    $this->content .= $course->courseid . $seperator . $usercount . $seperator . ''
-                        . $seperator . '' . $seperator . '' . $seperator;
-                }
-
-                // Get amount of answers (for each answerpossibility) for the current question.
-                $params = [
-                    "fid" => $this->feedback->id,
-                    "course" => $course->courseid,
-                    "qid" => $question->questionid
-                ];
-                $sql = "SELECT answer,COUNT(*) AS count
-                          FROM {block_coursefeedback_answers}
-                         WHERE coursefeedbackid = :fid 
-                               AND questionid = :qid 
-                               AND course = :course
-                      GROUP BY answer";
-
-                // Initiate (reset) $anserres
-                $answerres = array_fill($start_index, $num, $value);;
-                if ($results = $DB->get_records_sql($sql, $params)) {
-                    foreach ($results as $answer) {
-                        // Save the amount of times the answeroption was chosen at the correspending index of the $answerres-array.
-                        $answerres[$answer->answer] = $answer->count;
-                    }
-                    block_coursefeedback_array_fill_spaces($answerres, 0, 8, 0);
-                } else {
-                    $answerres = array_fill(0, 8, 0);
-                }
-                // Vsum -> Amount of given Answers
-                $vsum = 0;
-                for ($i = 1; $i <= 6; $i++) {
-                    $this->content .= $answerres[$i] . $seperator;
-                    $vsum += $i * $answerres[$i];
-                }
-                $answercount = array_sum($answerres);
-
-                // Ksum -> Amount of given Answers without the Amount of abstentions (abstentions were possible in earlier Versions).
-                $ksum = $answercount - $answerres[0];
-                $average = $ksum > 0 ? ($vsum / $ksum) : 0;
-
-                // Output
-                $this->content .= $average . $seperator . $answercount . $seperator . $answerres[0] . $newline;
+                $this->csvexportwriter->add_data((array) $course);
             }
-            $this->content .= $newline . $newline;
         }
-    }
-
-    public function get_content() {
-        return $this->content;
-    }
-
-    public function reset() {
-        $this->content = "";
+        $this->csvexportwriter->download_file();
     }
 }
---
+++ locallib.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Functions and classes for block management
+ *
+ * @package    block
+ * @subpackage coursefeedback
+ * @copyright  2023 Technische Universität Berlin
+ * @author     2011-2023 onwards Jan Eberhardt
+ * @author     2023 onwards Felix Di Lenarda
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+/**
+ * Returns the courserankings
+ *
+ * @param int $questionid
+ * @param int $coursefeedbackid
+ * @param int $showperpage
+ * @param int $page
+ * @return array of objects
+ * @throws \moodle_exception
+ */
+function block_coursefeedback_get_courserankings($questionid, $coursefeedbackid, $showperpage = 0, $page = 0) {
+    global $DB;
+    // Get courseids and the amount of answers in this course for the current question.
+    $params = [
+
+        'feedbackid' => $coursefeedbackid,
+        'feedbackid2' => $coursefeedbackid,
+        'questionid' => $questionid,
+        'questionid2' => $questionid,
+    ];
+    $sql = "
+        SELECT course.courseid, cenrol.enroleduserssum, c.shortname, c.category, cc.path,  
+            answer.one, answer.two, answer.three, answer.four, answer.five, answer.six, 
+            (answer.answersum / ( NULLIF (course.anstotal, 0))) as average, 
+            course.anstotal
+        FROM ( SELECT course as courseid, count(*) as anstotal 
+                FROM {block_coursefeedback_answers}
+                WHERE questionid = :questionid 
+                    AND coursefeedbackid = :feedbackid
+                GROUP BY course
+        ) course
+        -- Count the amount of users enrolled in each course --    
+        LEFT JOIN ( SELECT ce.courseid, SUM(users) as enroleduserssum 
+                        FROM ( SELECT enrol.id, enrol.courseid, userenrolments.users 
+                                FROM {enrol} enrol
+                                JOIN ( SELECT enrolid, COUNT(*) AS users 
+                                        FROM {user_enrolments}
+                                        GROUP BY enrolid 
+                                ) userenrolments ON enrol.id = userenrolments.enrolid    
+                        ) ce
+                        GROUP BY ce.courseid  
+        ) cenrol ON cenrol.courseid = course.courseid
+        -- Join the rest of the coursefields as c --    
+        LEFT JOIN {course} c ON course.courseid = c.id
+        -- Join the course category fields for each course --
+        LEFT JOIN {course_categories} cc ON cc.id = c.category
+        -- Join the calculated answers for each course --
+        LEFT JOIN ( SELECT course, 
+                        SUM(CASE WHEN answer = 1 THEN 1 ELSE 0 END) AS one,
+                        SUM(CASE WHEN answer = 2 THEN 1 ELSE 0 END) AS two,
+                        SUM(CASE WHEN answer = 3 THEN 1 ELSE 0 END) AS three,
+                        SUM(CASE WHEN answer = 4 THEN 1 ELSE 0 END) AS four,
+                        SUM(CASE WHEN answer = 5 THEN 1 ELSE 0 END) AS five,
+                        SUM(CASE WHEN answer = 6 THEN 1 ELSE 0 END) AS six,
+                        SUM(answer) AS answersum
+                    FROM {block_coursefeedback_answers}
+                    WHERE coursefeedbackid = :feedbackid2 
+                       AND questionid = :questionid2
+                    GROUP BY course
+        ) answer ON course.courseid = answer.course";
+
+    if ($showperpage != 0 && $page != 0) {
+        $limitnum = $showperpage * ($page -1);
+        $limitfrom = $showperpage;
+        $courserecords = $DB->get_records_sql($sql, $params, $limitfrom, $limitnum);
+
+    } else {
+        $courserecords = $DB->get_records_sql($sql, $params);
+
+    }
+    return $courserecords;
+}
