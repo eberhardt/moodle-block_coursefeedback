5bd47b972f4c632db7cc1d93b5376c0475270bcb
FelixDiLenarda <dilenarda@tu-berlin.de>
2024-01-25 16:27:05 +0100

    Fix former unwanted DB changes (#44)
    * Fix former unwanted DB changes
    
    * Fix DB mistakes from last version change
    
    * Update version.php
    
    Co-authored-by: Jan Eberhardt <jan.eberhardt@tu-berlin.de>
    
    * Remove DB prefix for index definition in upgrade.php
    
    ---------
    
    Co-authored-by: Felix Di Lenarda <lenarda@math.tu-berlin.de>
    Co-authored-by: Jan Eberhardt <jan.eberhardt@tu-berlin.de>

--- version.php
+++ version.php
@@ -27,8 +27,8 @@
 
 defined("MOODLE_INTERNAL") || die();
 
-$plugin->version = 2024015000;
+$plugin->version = 2024015001;
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "3.3.0 (Build: 2024012004)";
+$plugin->release = "3.3.1 (Build: 2024015001)";
 $plugin->component = "block_coursefeedback";
